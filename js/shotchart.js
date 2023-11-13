278b3083f778b760ac8ae42825e90fe4dbdc9bee
gnawman <haroldlander95@gmail.com>
2023-11-13 16:42:39 +1100

    refactoring code to prepare for makeChart()

--- js/shotchart.js
+++ js/shotchart.js
@@ -1,10 +1,36 @@
 function setup() {
     //setting up event listeners on every field so any change updates values
-    document.getElementById("toHit").addEventListener("input", numberWang);
-    document.getElementById("damageMod").addEventListener("input", numberWang);
-    document.getElementById("damageDice").addEventListener("input", numberWang);
-    document.getElementById("targetAC").addEventListener("input", numberWang);
-    document.getElementById("advSelect").addEventListener("change", numberWang);
+    document.getElementById("toHit").addEventListener("input", render);
+    document.getElementById("damageMod").addEventListener("input", render);
+    document.getElementById("damageDice").addEventListener("input", render);
+    document.getElementById("targetAC").addEventListener("input", render);
+    document.getElementById("advSelect").addEventListener("change", render);
+    render();
+}
+
+// called every time any field updates, calls numberwang, updates display, draws chart
+function render() {
+    //numberWang returns an array as follows [finalDamage,finalDamageSharp,hitChance,hitChanceSharp,critDamage]
+    var returns = numberWang();
+
+    document.getElementById("finalDamage").innerHTML = "average damage<br>no sharpshooter:<br>"+returns[0].toFixed(2);
+    document.getElementById("finalDamageSharp").innerHTML = "average damage<br>sharpshooter:<br>"+returns[1].toFixed(2);
+
+    if (returns[1] > returns[0]) {
+        sharpshootDisplay = "<h2 style='color:#70A288'>SHARPSHOOT</h2>";
+    } else {
+        sharpshootDisplay = "<h2 style='color:#B48EAE'> DON'T SHARPSHOOT</h2>";
+    };
+
+    document.getElementById("sharpshootDisplay").innerHTML = sharpshootDisplay;
+
+    var chart = makeFakeChart();
+    document.getElementById("chartDisplay").innerHTML = chart;
+
+    document.getElementById("hitChance").innerHTML = "normal hit chance: "+returns[2].toFixed(3);
+    document.getElementById("hitChanceSharp").innerHTML = "sharpshooter hit chance: "+returns[3].toFixed(3);
+    document.getElementById("critDamage").innerHTML = "weighted crit damage: "+returns[4].toFixed(3);
+
 }
 
 // calculates final hit chance using advantage state. Penalty should be -5 when sharpshooting
@@ -28,6 +54,7 @@ function getHitChance(toHit,toHitPenalty,targetAC,advState) {
     return hitChance;
 };
 
+// uses hardcoded crit chance to determine crit damage
 function getCritDamage(damageDice,damageMod,advState) {
     switch (advState) {
         case 0:
@@ -47,6 +74,7 @@ function getCritDamage(damageDice,damageMod,advState) {
     return critDamage;
 }
 
+//numberWang returns an array as follows [finalDamage,finalDamageSharp,hitChance,hitChanceSharp,critDamage]
 function numberWang() {
     // grabbing variables from input
     var toHit = +document.getElementById("toHit").value;
@@ -63,25 +91,39 @@ function numberWang() {
     // uses advantage state to get weighted crit damage
     var critDamage = getCritDamage(damageDice,damageMod,advState);
 
+    // calculates final returns
     var hitDamage = damageDice + damageMod;
     var finalDamage = (hitChance * hitDamage) + critDamage;
     var finalDamageSharp = (hitChanceSharp * (hitDamage+10)) + critDamage;
 
-    if (finalDamageSharp > finalDamage) {
-        sharpshootDisplay = "<h2 style='color:#70A288'>SHARPSHOOT</h2>";
-    } else {
-        sharpshootDisplay = "<h2 style='color:#B48EAE'> DON'T SHARPSHOOT</h2>";
-    };
-
-    document.getElementById("sharpshootDisplay").innerHTML = sharpshootDisplay;    
-    
-    document.getElementById("critDamage").innerHTML = "advantage-weighted crit damage: "+critDamage.toFixed(3);
-    document.getElementById("hitChance").innerHTML = "hit chance adjusted for advantage: "+hitChance.toFixed(3);
-    document.getElementById("finalDamage").innerHTML = "average damage: "+finalDamage.toFixed(3);
-
-    document.getElementById("hitChanceSharp").innerHTML = "sharpshooter hit chance: "+hitChanceSharp.toFixed(3);
-    document.getElementById("finalDamageSharp").innerHTML = "average sharpshooter damage: "+finalDamageSharp.toFixed(3);
+    return [finalDamage,finalDamageSharp,hitChance,hitChanceSharp,critDamage];
 };
+
+function makeFakeChart() {
+    var chart = "<svg height='500' width='500'>";
+    chart += "<line x1='0' y1='500' x2='0' y1='0' style='stroke:#84857E;stroke-width:2' />";
+    chart += "<line x1='0' y1='500' x2='500' y2='500' style='stroke:#84857E;stroke-width:2' />";
+    // generating y-axis scale lines
+    for (var i=450; i>-1; i-=50) {
+        chart += "<line x1='0' y1='" + i + "' x2='10' y2='" + i + "' style='stroke:#84857E;stroke-width:2' />";
+    }
+    // generating x-axis scale lines
+    for (var i=50; i<501; i+=50) {
+        chart += "<line x1='"+ i +"' y1='500' x2='"+ i +"' y2='490' style='stroke:#84857E;stroke-width:2' />";
+    }
+
+    var linepoints = [50,450,100,425,150,450,200,250,250,225,300,200,350,150,400,125,450,50];
+
+    chart += "<polyline points='"
+    // plotting line
+    for (var i=0; i<18; i++) {
+        chart += linepoints[i]+",";
+        i++;
+        chart +=linepoints[i]+" ";
+    };
+    chart += "' style='fill:none;stroke:#70A288;stroke-width:3' /></svg>";
+    return chart;
+}
 
 function accordion(elementId) {
     var x = document.getElementById(elementId);
