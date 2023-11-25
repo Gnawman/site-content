e063549da02146b7ed8febc94b751517efdf8e66
gnawman <haroldlander95@gmail.com>
2023-11-25 14:22:53 +1100

    first step for iterating over desires and roles

--- js/dh2e_aptitudes.js
+++ js/dh2e_aptitudes.js
@@ -1,15 +1,5 @@
 const aptitudeArray = ["weapon skill","ballistic skill","strength","toughness","agility","intelligence","perception","willpower","fellowship","offence","finesse","defence","psyker","tech","knowledge","leadership","fieldcraft","social"];
 
-const assassinBS = [["agility","ballistic skill","perception"] , ["fieldcraft","finesse"]];
-const assassinWS = [["agility","weapon skill","perception"] , ["fieldcraft","finesse"]];
-const chirurgeon = [["intelligence","strength","toughness"] , ["fieldcraft","knowledge"]];
-const desperado = [["agility","ballistic skill","defence"] , ["fellowship","finesse"]];
-const hierophant = [["fellowship","toughness","willpower"] , ["offence","social"]];
-const mystic = [["intelligence","perception","willpower"] , ["defence","knowledge"]];
-const sage = [["intelligence","perception","willpower"] , ["knowledge","tech"]];
-const seeker = [["fellowship","intelligence","perception"] , ["social","tech"]];
-const warrior = [["ballistic skill","strength","weapon skill"] , ["defence","offence"]];
-
 function setup() {
     var aptitudeGrid = document.getElementById("aptitudeGrid");
     for (var i = 0; i < aptitudeArray.length; i++) {
@@ -29,35 +19,48 @@ function setup() {
 };
 
 function submit() {
+    var assassinBS = [["agility","ballistic skill","perception"] , ["fieldcraft","finesse"]];
+    var assassinWS = [["agility","weapon skill","perception"] , ["fieldcraft","finesse"]];
+    var chirurgeon = [["intelligence","strength","toughness"] , ["fieldcraft","knowledge"]];
+    var desperado = [["agility","ballistic skill","defence"] , ["fellowship","finesse"]];
+    var hierophant = [["fellowship","toughness","willpower"] , ["offence","social"]];
+    var mystic = [["intelligence","perception","willpower"] , ["defence","knowledge"]];
+    var sage = [["intelligence","perception","willpower"] , ["knowledge","tech"]];
+    var seeker = [["fellowship","intelligence","perception"] , ["social","tech"]];
+    var warrior = [["ballistic skill","strength","weapon skill"] , ["defence","offence"]];
     var roleArray = [assassinBS, assassinWS, chirurgeon, desperado, hierophant, mystic, sage, seeker, warrior];
     var roleArrayNames = ["assassin (ballistic skill)","assassin (weapon skill)","chirurgeon","desperado","hierophant","mystic","sage","seeker","warrior"];
+    
     var desires = [];
+
     var checkedBoxes = document.getElementsByClassName("attributeCheckbox");
     for (var i = 0; i < checkedBoxes.length; i++) {
         if (checkedBoxes[i].checked == true) {
             desires.push(checkedBoxes[i].id);
         };
     };
- 
+    
     if (document.getElementById("psykerCheck").checked == true) {
         roleArray.splice(5,1);
         roleArrayNames.splice(5,1);
     };
 
-    var matches = [];
-
-    for (var i = 0; i < desires.length; i++) {
-        var roleMatch = [];
-        for (var j = 0; j < roleArray.length; j++) {
-            if (roleArray[j][0].includes(desires[i])) {
-                roleMatch.push(desires[i]);
+    for (var i = 0; i < roleArray.length; i++) {
+        for (var j = 0; j < desires.length; j++) {
+            if (roleArray[i][0].includes(desires[j])) {
+                var index = roleArray[i][0].indexOf(desires[j]);
+                roleArray[i][0].splice(index,1);
             };
-            if (roleArray[j][1].includes(desires[i])) {
-                roleMatch.push(desires[i]);
+            if (roleArray[i][1].includes(desires[j])) {
+                var index = roleArray[i][1].indexOf(desires[j]);
+                roleArray[i][1].splice(index,1);
             };
         };
-        matches.push(roleMatch);
-        console.log(roleMatch);
     };
-    console.log(matches);
+
+    for (var i = 0; i < roleArray.length; i++) {
+        if (roleArray[i][0].length == 0 && roleArray[i][1].length ==0 ) {
+            console.log(roleArrayNames[i]);
+        };
+    };
 };
