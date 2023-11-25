47f10f33cbf846ed5cde0834da9dee6aac1b62b3
gnawman <haroldlander95@gmail.com>
2023-11-25 12:32:23 +1100

    split role atts into characteristics and other

--- js/dh2e_aptitudes.js
+++ js/dh2e_aptitudes.js
@@ -1,13 +1,14 @@
 const aptitudeArray = ["weapon skill","ballistic skill","strength","toughness","agility","intelligence","perception","willpower","fellowship","offence","finesse","defence","psyker","tech","knowledge","leadership","fieldcraft","social"];
-const assassinBS = ["agility","ballistic skill","fieldcraft","finesse","perception"];
-const assassinWS = ["agility","weapon skill","fieldcraft","finesse","perception"];
-const chirurgeon = ["fieldcraft","intelligence","knowledge","strength","toughness"];
-const desperado = ["agility","ballistic skill","defence","fellowship","finesse"];
-const hierophant = ["fellowship","offence","social","toughness","willpower"];
-const mystic = ["defence","intelligence","knowledge","perception","willpower"];
-const sage = ["intelligence","knowledge","perception","tech","willpower"];
-const seeker = ["fellowshop","intelligence","perception","social","tech"];
-const warrior = ["ballistic skill","defence","offence","strength","weapon skill"];
+
+const assassinBS = [["agility","ballistic skill","perception"] , ["fieldcraft","finesse"]];
+const assassinWS = [["agility","weapon skill","perception"] , ["fieldcraft","finesse"]];
+const chirurgeon = [["intelligence","strength","toughness"] , ["fieldcraft","knowledge"]];
+const desperado = [["agility","ballistic skill","defence"] , ["fellowship","finesse"]];
+const hierophant = [["fellowship","toughness","willpower"] , ["offence","social"]];
+const mystic = [["intelligence","perception","willpower"] , ["defence","knowledge"]];
+const sage = [["intelligence","perception","willpower"] , ["knowledge","tech"]];
+const seeker = [["fellowship","intelligence","perception"] , ["social","tech"]];
+const warrior = [["ballistic skill","strength","weapon skill"] , ["defence","offence"]];
 
 function setup() {
     var aptitudeGrid = document.getElementById("aptitudeGrid");
@@ -24,21 +25,39 @@ function setup() {
         newDiv.append(newInput);
         newDiv.append(newLabel);
         aptitudeGrid.append(newDiv);
-    }
-}
+    };
+};
 
 function submit() {
     var roleArray = [assassinBS, assassinWS, chirurgeon, desperado, hierophant, mystic, sage, seeker, warrior];
-    var desired = [];
-    var elements = document.getElementsByClassName("attributeCheckbox");
-    for (var i = 0; i < elements.length; i++) {
-        if (elements[i].checked == true) {
-            desired.push(elements[i].id);
+    var roleArrayNames = ["assassin (ballistic skill)","assassin (weapon skill)","chirurgeon","desperado","hierophant","mystic","sage","seeker","warrior"];
+    var desires = [];
+    var checkedBoxes = document.getElementsByClassName("attributeCheckbox");
+    for (var i = 0; i < checkedBoxes.length; i++) {
+        if (checkedBoxes[i].checked == true) {
+            desires.push(checkedBoxes[i].id);
         };
     };
-    console.log("desired: "+desired);
+ 
     if (document.getElementById("psykerCheck").checked == true) {
         roleArray.splice(5,1);
+        roleArrayNames.splice(5,1);
     };
-    console.log(roleArray);
-}
+
+    var matches = [];
+
+    for (var i = 0; i < desires.length; i++) {
+        var roleMatch = [];
+        for (var j = 0; j < roleArray.length; j++) {
+            if (roleArray[j][0].includes(desires[i])) {
+                roleMatch.push(desires[i]);
+            };
+            if (roleArray[j][1].includes(desires[i])) {
+                roleMatch.push(desires[i]);
+            };
+        };
+        matches.push(roleMatch);
+        console.log(roleMatch);
+    };
+    console.log(matches);
+};
