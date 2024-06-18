0e2c39df70179c2a24add712183ccf604adc6743
gnawman <haroldlander95@gmail.com>
2024-06-19 00:25:21 +1000

    spice now balanced between first and second names

--- js/phonames.js
+++ js/phonames.js
@@ -2,8 +2,12 @@ function returnPhoname() {
     // TODO replace this with reading from a dropdown or button
     let spice = document.getElementById("spice").value;
 
-    let firstName = constructPhoname(spice);
-    let secondName = constructPhoname(spice);
+    let spiceAdj = Math.random()+0.5;
+    let firstNameSpice = spice*spiceAdj;
+    let secondNameSpice = spice/spiceAdj;
+
+    let firstName = constructPhoname(firstNameSpice);
+    let secondName = constructPhoname(secondNameSpice);
     let fullName = firstName.concat(" ", secondName);
 
     document.getElementById('namedisplay').innerHTML = fullName;
@@ -11,80 +15,60 @@ function returnPhoname() {
 
 function constructPhoname(spice) {
     let name = [];
-    let phonemeSeed = Math.floor(Math.random()*spice);
-    console.log(phonemeSeed);
-
-    if (phonemeSeed > 120) {
-        name+=phoneme(0);
-        name+=phoneme(0);
-        name+=phoneme(4);
-        name+=phoneme(4);
-        if (Math.random() < 0.4) {
-            name+=phoneme(6);
-        }
-    } else if (phonemeSeed > 80) {
-        name+=phoneme(0);
-        name+=phoneme(1);
-        name+=phoneme(4);
-        if (Math.random() < 0.4) {
-            name+=phoneme(6);
-        }
-    } else if (phonemeSeed > 60) {
-        name+=phoneme(0);
-        name+=phoneme(2);
-        name+=phoneme(6);
-        if (Math.random() < 0.7) {
-            name+=phoneme(5);
-        }
-    } else if (phonemeSeed > 30) {
-        name+=phoneme(2);
-        name+=phoneme(3);
-    } else if (phonemeSeed > 5) {
-        name+=phoneme(2);
-    } else {
-        name+=phoneme(4);
-    }
-
-    return name;
-}
-
-function phoneme(phonemeCase) {
-    let phoneme = [];
+    let nameSpice = 0;
     let consonants = ["b","d","f","g","h","l","m","n","p","r","s","t"];
-    let consonantsSpice = ["b","d","f","g","h","l","m","n","p","r","s","t","y","ng","zh","ch","sh","th","z","w","v","j","k"];
+    let consonantsSpice = ["b","d","f","g","h","l","m","n","p","r","s","t","y","ch","sh","th","z","w","v","j","k"];
     let vowels = ["a","e","i","o","u"];
+    let consonantStreak = 1;
+    let vowelStreak = 1;
+    console.log(spice);
 
-    switch (phonemeCase) {
-        case 0:
-            phoneme+=consonantsSpice[Math.floor(Math.random()*consonantsSpice.length)];
-            phoneme+=vowels[Math.floor(Math.random() * vowels.length)];
-            phoneme+=consonantsSpice[Math.floor(Math.random()*consonantsSpice.length)];
-            break;
-        case 1:
-            phoneme+=consonantsSpice[Math.floor(Math.random()*consonantsSpice.length)];
-            phoneme+=vowels[Math.floor(Math.random() * vowels.length)];
-            phoneme+=consonants[Math.floor(Math.random()*consonants.length)];
-            break;
-        case 2:
-            phoneme+=consonants[Math.floor(Math.random()*consonants.length)];
-            phoneme+=vowels[Math.floor(Math.random() * vowels.length)];
-            phoneme+=consonants[Math.floor(Math.random()*consonants.length)];
-            break;
-        case 3:
-            phoneme+=consonants[Math.floor(Math.random()*consonants.length)];
-            phoneme+=vowels[Math.floor(Math.random() * vowels.length)];
-            break;
-        case 4:
-            phoneme+=vowels[Math.floor(Math.random() * vowels.length)];
-            phoneme+=consonants[Math.floor(Math.random()*consonants.length)];
-            break;
-        case 5:
-            phoneme+=consonants[Math.floor(Math.random()*consonants.length)];
-            break;
-        case 6:
-            phoneme+=vowels[Math.floor(Math.random() * vowels.length)];
-            break;
+    while (nameSpice < spice) {
+        if (consonantStreak > 1) {
+            name+=vowels[Math.floor(Math.random() * vowels.length)];
+            consonantStreak = 0;
+            vowelStreak = 1;
+            nameSpice += 25;
+        }
+        if (vowelStreak > 1) {
+            name+=consonants[Math.floor(Math.random()*consonants.length)];
+            vowelStreak = 0;
+            consonantStreak = 1;
+            nameSpice += 30;
+        }
+
+        if (vowelStreak == 1) {
+            if (Math.random() < 0.9) {
+                name+=consonantsSpice[Math.floor(Math.random()*consonantsSpice.length)];
+                vowelStreak = 0;
+                consonantStreak += 1;
+                nameSpice += 20;
+            } else {
+                name+=vowels[Math.floor(Math.random() * vowels.length)];
+                consonantStreak = 0;
+                vowelStreak += 1;
+                nameSpice += 35;
+            }
+        }
+
+        if (consonantStreak == 1) {
+            if (Math.random() < 0.9) {
+                name+=vowels[Math.floor(Math.random() * vowels.length)];
+                consonantStreak = 0;
+                vowelStreak += 1;
+                nameSpice += 20;
+            } else {
+                name+=consonants[Math.floor(Math.random()*consonants.length)];
+                vowelStreak = 0;
+                consonantStreak += 1;
+                nameSpice += 35;
+            }
+        }
     }
 
-    return phoneme;
+    if (name.length == 1) {
+        name+=vowels[Math.floor(Math.random() * vowels.length)];
+        name+=consonants[Math.floor(Math.random()*consonants.length)];
+    }
+    return name;
 }
