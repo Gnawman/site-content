8c6e78cc1f98dd4d0d66cab1d4b880fe22d8cad9
gnawman <haroldlander95@gmail.com>
2025-04-11 18:57:14 +1000

    small refactor creating functions

--- js/aurebesh.js
+++ js/aurebesh.js
@@ -1,12 +1,16 @@
 function getword() {
     //read the hidden wordlist paragraph and split on whitespace
     let wordlistArray = document.getElementById('wordlist').innerHTML.split(',');
-    //pick a random item from the array, doing numberwang to adjust for length of array
-    let wordlistOutput = wordlistArray[Math.floor(Math.random()*wordlistArray.length)];
+
+    //pick a random item from the array, do numberwang to adjust for length of array, then replace a hidden div
+    let chosenWord = wordlistArray[Math.floor(Math.random()*wordlistArray.length)];
+    document.getElementById('chosenword')
+    
     let spice = document.getElementById("spice").value;
 
     //replace the placeholder worddisplay <p> with output
-    document.getElementById('worddisplay').innerHTML = scrample(wordlistOutput, spice);
+    document.getElementById('worddisplay').innerHTML = scrample(chosenWord, spice);
+
 }
 
 function scrample(wordInput, spice) {
@@ -19,7 +23,10 @@ function scrample(wordInput, spice) {
             output+='<span>'+wordInput.charAt(i)+'</span>'+" "
             console.log(output);
         }
-        
     }
     return output;
+}
+
+function checkText() {
+    let textInput = ""
 }
