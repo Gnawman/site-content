cd8c7a5ad5256482964977f4c16b9e7cdfbccb86
gnawman <haroldlander95@gmail.com>
2025-04-13 19:59:43 +1000

    minimum viable aurebeshucation

---
+++ js/aurebeshucation.js
@@ -0,0 +1,105 @@
+function getWord() {
+    let wordListArray = document.getElementById('wordlist').innerHTML.split(/[\r\n]+/);
+    let chosenWord = wordListArray[Math.floor(Math.random()*wordListArray.length)];
+    let output = chosenWord.split("");
+    return output;
+};
+
+function makeScrampleArray(wordArray, spice) {
+    let output = "";
+    for (var i=0; i<wordArray.length; i++) {
+        if (Math.random()*100 < spice) {
+            output+="Y";
+        } else {
+            output+="N";
+        };
+    };
+    return output;
+};
+
+function renderWord() {
+    let wordDisplay = document.getElementById("worddisplay");
+    wordDisplay.innerHTML = '';
+    let wordArray = getWord();
+    let spice = document.getElementById("spice").value;
+    let scrampleArray = makeScrampleArray(wordArray,spice);
+    for (var i=0; i<wordArray.length; i++) {
+        let letter = document.createElement("span")
+        letter.innerText = wordArray[i];
+        if (scrampleArray[i] == "Y") {
+            letter.classList.add("aurebesh")
+            letter.classList.add("feature-colour")
+        };
+        wordDisplay.appendChild(letter);
+    };
+};
+
+function checkText() {
+    var wordDisplay = document.getElementById("worddisplay").children;
+    var typedText = document.getElementById("typedtext").value;
+    for (var i=0; i<wordDisplay.length; i++) {
+        wordDisplay[i].classList.remove("feature-colour");
+        wordDisplay[i].classList.remove("aurebeshucation-correct");
+        wordDisplay[i].classList.remove("aurebeshucation-incorrect");
+
+        console.log(wordDisplay[i].innerText);
+        console.log(typedText[i]);
+        
+        if (wordDisplay[i].innerText == typedText[i]) {
+            wordDisplay[i].classList.add("aurebeshucation-correct");
+        } else {
+            wordDisplay[i].classList.add("aurebeshucation-incorrect");
+        };
+    };
+};
+
+/*
+function getWord() {
+    //read the hidden wordlist paragraph and split on whitespace
+    let wordlistArray = document.getElementById('wordlist').innerHTML.split(/[\r\n]+/);
+
+    //pick a random item from the array, do numberwang to adjust for length of array, then replace a hidden div
+    let chosenWord = wordlistArray[Math.floor(Math.random()*wordlistArray.length)];
+    document.getElementById('chosenword').innerHTML = chosenWord
+    
+    let spice = document.getElementById("spice").value;
+
+    document.getElementById('input').addEventListener("keypress", function(event) {
+        // If the user presses the "Enter" key on the keyboard
+        if (event.key === "Enter") {
+          // Cancel the default action, if needed
+          event.preventDefault();
+          // Trigger the button element with a click
+          document.getElementById("checkText").click();
+        }
+    }); 
+
+    //replace the placeholder worddisplay <p> with output
+    document.getElementById('worddisplay').innerHTML = scrample(chosenWord, spice);
+
+}
+
+function scrample(wordInput, spice) {
+    let output = "";
+    for (var i=0; i<wordInput.length; i++) {
+        if (Math.random()*100 < spice) {
+            output+='<span style = "font-family:aurebesh">'+wordInput.charAt(i)+'</span>'+" ";
+        } else {
+            output+='<span>'+wordInput.charAt(i)+'</span>'+" ";
+        }
+    }
+    return output;
+}
+
+function checkText() {
+    let textInput = document.getElementById('input').value;
+    let chosenWord = document.getElementById('chosenword').innerHTML;
+    console.log(textInput);
+    console.log(chosenWord);
+    if (textInput == chosenWord) {
+        document.getElementById('worddisplay').style["color"] = "#70A288";
+    } else {
+        document.getElementById('worddisplay').style["color"] = "#B48EAE";
+    }
+}
+*/
