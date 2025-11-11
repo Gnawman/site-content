7fd9a5e2915c315b7b8786e4080e68ab74658d5a
gnawman <haroldlander95@gmail.com>
2025-11-11 15:08:42 +1100

    blood bath and token functionality

--- js/trenchcrusalculator.js
+++ js/trenchcrusalculator.js
@@ -7,6 +7,10 @@ function crusalculate() {
     let injureDiceKeep = +document.getElementById("injureDiceKeep").value;
     let injureDiceNeg = +document.getElementById("injureHighLow").selectedIndex;
     let injureModifier = +document.getElementById("injureModifier").value;
+    let bloodCheck1 = document.getElementById("bloodCheck1");
+    let bloodCheck2 = document.getElementById("bloodCheck2");
+    let bloodCheck3 = document.getElementById("bloodCheck3");
+    let bloodCheck4 = document.getElementById("bloodCheck4");
 
     let diceSides = 6;
 
@@ -14,19 +18,12 @@ function crusalculate() {
     let attackDiceAmount = Math.abs(attackDiceAmountInput);
     let attackHighestRoll = diceSides * attackDiceKeep;
     let injureDiceAmount = Math.abs(injureDiceAmountInput);
-    let injureHighestRoll = (diceSides * injureDiceKeep)+injureModifier;
 
     // setup variables
     let depth = 0;
     let attackRolls = [];
-    let injureRolls = [];
     let hitChanceDict = {};
-    let injureChanceDict = {};
     let hitChance = 0;
-    let nothingChance = 0;
-    let minorHitChance = 0;
-    let downChance = 0;
-    let outChance = 0;
 
     // construct the array that will be iterated over in rollDice
     let attackDiceArray = [];
@@ -34,17 +31,9 @@ function crusalculate() {
         attackDiceArray.push(diceSides);
     };
 
-    let injureDiceArray = [];
-    for (let i = 0; i < injureDiceAmount; i++) {
-        injureDiceArray.push(diceSides);
-    };
-
-    // this generates a list of all possible rolls from the amount of dice, keeping the highest or lowest amount specified
+    // this generates a list of all possible attack rolls from the amount of dice, keeping the highest or lowest amount specified
     let attackSampleSpace = rollDice(attackDiceAmount,attackDiceNeg,attackDiceKeep,diceSides,attackDiceArray,0,depth,attackRolls)[1];
     let attackSampleSpaceLength = attackSampleSpace.length;
-
-    let injureSampleSpace = rollDice(injureDiceAmount,injureDiceNeg,injureDiceKeep,diceSides,injureDiceArray,injureModifier,depth,injureRolls)[1];
-    let injureSampleSpaceLength = injureSampleSpace.length;
 
     // lists every possible number for attacks, and the probability of rolling that number
     for (let i = attackDiceKeep; i < attackHighestRoll+1; i++){
@@ -54,32 +43,42 @@ function crusalculate() {
         };
     };
 
-    // lists every possible number for injuries, and the probability of rolling that number
-    for (let i = injureDiceKeep+injureModifier; i < injureHighestRoll+1; i++){
-        injureChanceDict[i] = countOccurrences(injureSampleSpace,i)/injureSampleSpaceLength;
-        if (i < 2) {
-            nothingChance = nothingChance + Number(injureChanceDict[i]);
-        } else if (i > 1 && i < 7) {
-            minorHitChance = minorHitChance + Number(injureChanceDict[i]);
-        } else if (i > 6 && i < 9) {
-            downChance = downChance + Number(injureChanceDict[i]);
-        } else {
-            outChance = outChance + Number(injureChanceDict[i]);
-        };
+    // calculates and displays injury table for vanilla roll
+    injure(0,injureDiceAmount,injureDiceNeg,injureDiceKeep,diceSides,injureModifier,depth,hitChance);
+
+    // displays the extended table if any boxes are checked
+    if (bloodCheck1.checked || bloodCheck2.checked || bloodCheck3.checked || bloodCheck4.checked) {
+        document.getElementById("tableBlood").style.display = "inline";
+    } else {
+        document.getElementById("tableBlood").style.display = "none"
     };
 
-    // TODO output for testing
-    console.log(hitChanceDict);
-    console.log(injureChanceDict);
-    // document.getElementById("hitChance").innerText = "hit chance "+(hitChance*100).toFixed(2)+"%";
-    document.getElementById("nothingChance").innerText = ((100-hitChance*100)+(nothingChance*hitChance*100)).toFixed(2)+"%";
-    document.getElementById("minorHitChance").innerText = (minorHitChance*hitChance*100).toFixed(2)+"%";
-    document.getElementById("downChance").innerText = (downChance*hitChance*100).toFixed(2)+"%";
-    document.getElementById("outChance").innerText = (outChance*hitChance*100).toFixed(2)+"%";
-    
+    // displays only the rows the user has checked
+    tableRowDisplay(bloodCheck1);
+    tableRowDisplay(bloodCheck2);
+    tableRowDisplay(bloodCheck3);
+    tableRowDisplay(bloodCheck4);
+
+    // calculates and displays injury table for any ticked modifiers
+    if (bloodCheck1.checked) {
+        injure(1,injureDiceAmount,injureDiceNeg,injureDiceKeep,diceSides,injureModifier,depth,hitChance);
+    };
+
+    if (bloodCheck2.checked) {
+        injure(2,injureDiceAmount,injureDiceNeg,injureDiceKeep,diceSides,injureModifier,depth,hitChance);
+    };
+
+    if (bloodCheck3.checked) {
+        injure(3,injureDiceAmount,injureDiceNeg,injureDiceKeep,diceSides,injureModifier,depth,hitChance);
+    };
+
+    if (bloodCheck4.checked) {
+        injure(4,injureDiceAmount,injureDiceNeg,injureDiceKeep,diceSides,injureModifier,depth,hitChance);
+    };
+
 };
 
-// this is where the magic happens
+// takes the user-selected parameters and outputs a sample space of all possible rolls
 function rollDice(diceAmount,diceNeg,diceKeep,diceSides,diceArray,injureModifier,depth,rolls) {
     // first set up a loop that runs a number of times equal to the size of the dice
     for (let i = 0; i < diceSides; i++) {
@@ -113,10 +112,7 @@ function rollDice(diceAmount,diceNeg,diceKeep,diceSides,diceArray,injureModifier
             // applies modifier and appends the result to the rolls variable
             // modifier set to zero when doing to-hit rolls
             rolls.push(sum+injureModifier);
-
-            // TODO output for testing
-            console.log("dice array "+diceArray+" | working dice array "+workingDiceArray);
-        }
+        };
 
         // decrement a single die in the array, selected by the current depth
         diceArray[depth] = diceArray[depth]-1;
@@ -135,4 +131,70 @@ function countOccurrences(array, value) {
       let count = 0;
       array.forEach((v) => (v === value && count++));
       return count;
-  }
+};
+
+// takes the big injury chance sample space and condenses it into four breakpoints: nothing, minor hit, down, and out
+function injuryProbability(injureLowestRoll,injureHighestRoll,injureSampleSpace) {
+    let nothingChance = 0;
+    let minorHitChance = 0;
+    let downChance = 0;
+    let outChance = 0;
+
+    for (let i = injureLowestRoll; i < injureHighestRoll+1; i++){
+        let injureChanceDict = {};
+        injureChanceDict[i] = countOccurrences(injureSampleSpace,i)/injureSampleSpace.length;
+        if (i < 2) {
+            nothingChance = nothingChance + Number(injureChanceDict[i]);
+        } else if (i > 1 && i < 7) {
+            minorHitChance = minorHitChance + Number(injureChanceDict[i]);
+        } else if (i > 6 && i < 9) {
+            downChance = downChance + Number(injureChanceDict[i]);
+        } else {
+            outChance = outChance + Number(injureChanceDict[i]);
+        };
+    };
+    return [nothingChance,minorHitChance,downChance,outChance]
+};
+
+// checks if checkbox is checked and displays row if it is
+function tableRowDisplay(rowCheck) {
+    let row = document.getElementById("tableRowBlood"+rowCheck.id.slice(10));
+    if (rowCheck.checked) {
+        row.style.display = "table-row";
+    } else {
+        row.style.display = "none";
+    };
+};
+
+// takes user-selected parameters as well as hit chance, calls rollDice() to generate sample space, condenses sample space with injuryProbability(), and writes output to page
+// bloodChoice is 0:vanilla, 1-3:number of blood tokens spent, 4:bloodbath
+function injure(bloodChoice,injureDiceAmount,injureDiceNeg,injureDiceKeep,diceSides,injureModifier,depth,hitChance) {
+    let injureDiceAmountMod = bloodChoice;
+    let injureDiceKeepMod = 0;
+
+    // checks if bloodbath is selected
+    if (bloodChoice == 4 ) {
+        injureDiceAmountMod = 1;
+        injureDiceKeepMod = 1;
+    };
+
+    let injureHighestRoll = (diceSides * (injureDiceKeep+injureDiceKeepMod)) + injureModifier;
+    let injureLowestRoll = injureDiceKeep+injureDiceKeepMod + injureModifier;
+
+    let injureDiceArray = [];
+    for (let i = 0; i < injureDiceAmount+injureDiceAmountMod; i++) {
+        injureDiceArray.push(diceSides);
+    };
+
+    // setting up variables to pass to 
+    let injureRolls = [];
+
+    let injureSampleSpace = rollDice(injureDiceAmount+injureDiceAmountMod,injureDiceNeg,injureDiceKeep+injureDiceKeepMod,diceSides,injureDiceArray,injureModifier,depth,injureRolls)[1];
+
+    [nothingChance,minorHitChance,downChance,outChance] = injuryProbability(injureLowestRoll,injureHighestRoll,injureSampleSpace);
+
+    document.getElementById("nothingChance"+bloodChoice).innerText = ((100-hitChance*100)+(nothingChance*hitChance*100)).toFixed(2)+"%";
+    document.getElementById("minorHitChance"+bloodChoice).innerText = (minorHitChance*hitChance*100).toFixed(2)+"%";
+    document.getElementById("downChance"+bloodChoice).innerText = (downChance*hitChance*100).toFixed(2)+"%";
+    document.getElementById("outChance"+bloodChoice).innerText = (outChance*hitChance*100).toFixed(2)+"%";
+};
